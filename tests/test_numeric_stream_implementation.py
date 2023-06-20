4865d2cb2ec611a79dd954147a991d1b4cdfa050
Stefan Garlonta <stefan.garlonta@gmail.com>
2023-06-20 14:03:41 +0200

    Add `sum()` operation to numerical streams

--- tests/test_numeric_stream_implementation.py
+++ tests/test_numeric_stream_implementation.py
@@ -23,3 +23,11 @@ class TestNumericStreamImplementation(TestCase):
     def test_mean_negative(self):
         result = self.stream([-1, -2, -3, -4, -5]).mean()
         self.assertEqual(result, -3)
+
+    def test_sum(self):
+        result = self.stream([1, 2, 3, 4, 5]).sum()
+        self.assertEqual(result, 15)
+
+    def test_sum_empty(self):
+        result = self.stream([]).sum()
+        self.assertEqual(result, 0)
