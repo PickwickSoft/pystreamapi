1dde797a8dec891f08b1c6d894311a04d6517731
Stefan Garlonta <stefan.garlonta@gmail.com>
2023-07-25 16:02:55 +0200

    :bookmark: Change version to 1.0.0

--- pystreamapi/__init__.py
+++ pystreamapi/__init__.py
@@ -1,5 +1,5 @@
 from pystreamapi.__stream import Stream
 from pystreamapi._streams.error.__levels import ErrorLevel
 
-__version__ = "0.3.3"
+__version__ = "1.0.0"
 __all__ = ["Stream", "ErrorLevel"]
